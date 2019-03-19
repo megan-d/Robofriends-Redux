324523e3ec6c6da8a1e9863edf5289ddb28621e0
Megan D <45672063+megan-d@users.noreply.github.com>
2019-03-19 06:30:24 -0600

    create actions and constants files

---
+++ src/actions.js
@@ -0,0 +1,10 @@
+import { CHANGE_SEARCH_FIELD } from './constants.js';
+
+
+//CREATE OUR ACTION
+//wrap in () so can avoid return statement (just returning an object)
+//takes text and returns an object with a type of change_search_field and send whatever data is needed to go on to reducer
+export const setSearchField = (text) => ({
+    type: CHANGE_SEARCH_FIELD,
+    payload: text
+})
